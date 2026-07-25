fa6f6770f401f80651c2e8853fc567c4c60ef510
apelosii <aidanpelosi13@outlook.com>
2026-07-25 18:13:56 -0400

    change: refactored front end

--- ADSB-FrontEnd/UdpReciever.hpp
+++ ADSB-FrontEnd/UdpReciever.hpp
@@ -4,191 +4,115 @@
 #include <stdexcept>
 
 #ifdef _WIN32
-
 #include <winsock2.h>
 #include <ws2tcpip.h>
-
 #else
-
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-
 #endif
 
 #include "../Debug.hpp"
 
-class UdpReceiver
-{
+
+class UdpReceiver{
 private:
-
 #ifdef _WIN32
-
     SOCKET socket_fd = INVALID_SOCKET;
-
 #else
-
     int socket_fd = -1;
-
 #endif
 
 public:
-
-    explicit UdpReceiver(
-        int port
-    )
-    {
+    explicit UdpReceiver(int port){
 
 #ifdef _WIN32
-
         WSADATA wsa_data{};
 
-        if (
-            WSAStartup(
-                MAKEWORD(2, 2),
-                &wsa_data
-            ) != 0
-        )
-        {
-            throw std::runtime_error(
-                "WSAStartup failed"
-            );
+        if(WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0){
+            throw std::runtime_error("WSAStartup failed");
         }
-
 #endif
 
-        socket_fd =
-            socket(
-                AF_INET,
-                SOCK_DGRAM,
-                IPPROTO_UDP
-            );
+        socket_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
 #ifdef _WIN32
-
-        if (socket_fd == INVALID_SOCKET)
-
+        if(socket_fd == INVALID_SOCKET){
 #else
-
-        if (socket_fd < 0)
-
+        if(socket_fd < 0){
 #endif
-
-        {
 
 #ifdef _WIN32
-
             WSACleanup();
-
 #endif
 
-            throw std::runtime_error(
-                "Failed to create UDP socket"
-            );
+            throw std::runtime_error("Failed to create UDP socket");
         }
 
         sockaddr_in address{};
 
-        address.sin_family =
-            AF_INET;
+        address.sin_family = AF_INET;
+        address.sin_port = htons(port);
+        address.sin_addr.s_addr = INADDR_ANY;
 
-        address.sin_port =
-            htons(port);
-
-        address.sin_addr.s_addr =
-            INADDR_ANY;
-
-        if (
-            bind(
-                socket_fd,
-                reinterpret_cast<sockaddr*>(
-                    &address
-                ),
-                sizeof(address)
-            )
-
-#ifdef _WIN32
-
-            == SOCKET_ERROR
-
-#else
-
-            < 0
-
-#endif
-
+        if(bind(
+            socket_fd,
+            reinterpret_cast<sockaddr*>(&address),
+            sizeof(address)
         )
-        {
+#ifdef _WIN32
+        == SOCKET_ERROR
+#else
+        < 0
+#endif
+        ){
 
 #ifdef _WIN32
-
             closesocket(socket_fd);
             WSACleanup();
-
 #else
-
             close(socket_fd);
-
 #endif
 
-            throw std::runtime_error(
-                "Failed to bind UDP socket"
-            );
+            throw std::runtime_error("Failed to bind UDP socket");
         }
 
-        Debug::success(
-            "UdpReceiver",
-            "Listening on UDP port "
-            + std::to_string(port)
-        );
+        Debug::success("UdpReceiver", "Listening on UDP port " + std::to_string(port));
     }
 
-    ~UdpReceiver()
-    {
+
+    ~UdpReceiver(){
 
 #ifdef _WIN32
-
-        if (socket_fd != INVALID_SOCKET)
-        {
+        if(socket_fd != INVALID_SOCKET){
             closesocket(socket_fd);
         }
 
         WSACleanup();
-
 #else
-
-        if (socket_fd >= 0)
-        {
+        if(socket_fd >= 0){
             close(socket_fd);
         }
-
 #endif
 
-        Debug::log(
-            "UdpReceiver",
-            "Socket closed"
-        );
+        Debug::log("UdpReceiver", "Socket closed");
     }
 
-    std::string read_line()
-    {
+
+    std::string read_line(){
         char buffer[2048];
 
-        int bytes_received =
-            recvfrom(
-                socket_fd,
-                buffer,
-                sizeof(buffer) - 1,
-                0,
-                nullptr,
-                nullptr
-            );
+        int bytes_received = recvfrom(
+            socket_fd,
+            buffer,
+            sizeof(buffer) - 1,
+            0,
+            nullptr,
+            nullptr
+        );
 
-        if (bytes_received <= 0)
-        {
-            throw std::runtime_error(
-                "Failed to receive UDP packet"
-            );
+        if(bytes_received <= 0){
+            throw std::runtime_error("Failed to receive UDP packet");
         }
 
         buffer[bytes_received] = '\0';
--- ADSB-FrontEnd/main.cpp
+++ ADSB-FrontEnd/main.cpp
@@ -9,276 +9,129 @@
 #include "UdpReciever.hpp"
 
 
-void clear_screen()
-{
-    std::cout
-        << "\033[2J\033[1;1H";
+void clear_screen(){
+    std::cout << "\033[2J\033[1;1H";
 }
 
 
-void print_aircraft(
-    const Aircraft& aircraft
-)
-{
-    std::cout
-        << "-----------------------------\n";
+void print_aircraft(const Aircraft& aircraft){
+    std::cout << "-----------------------------\n";
+    std::cout << "ICAO: " << aircraft.hex_ident << '\n';
 
+    std::cout << "Callsign: ";
 
-    std::cout
-        << "ICAO: "
-        << aircraft.hex_ident
-        << '\n';
-
-
-    std::cout
-        << "Callsign: ";
-
-    if (
-        aircraft.callsign.has_value()
-    )
-    {
-        std::cout
-            << aircraft.callsign.value();
+    if(aircraft.callsign.has_value()){
+        std::cout << aircraft.callsign.value();
     }
-    else
-    {
-        std::cout
-            << "Unknown";
+    else{
+        std::cout << "Unknown";
     }
 
-    std::cout
-        << '\n';
+    std::cout << '\n';
 
+    std::cout << "Altitude: ";
 
-    std::cout
-        << "Altitude: ";
-
-    if (
-        aircraft.altitude.has_value()
-    )
-    {
-        std::cout
-            << aircraft.altitude.value()
-            << " ft";
+    if(aircraft.altitude.has_value()){
+        std::cout << aircraft.altitude.value() << " ft";
     }
-    else
-    {
-        std::cout
-            << "Unknown";
+    else{
+        std::cout << "Unknown";
     }
 
-    std::cout
-        << '\n';
+    std::cout << '\n';
 
+    std::cout << "Position: ";
 
-    std::cout
-        << "Position: ";
-
-    if (
-        aircraft.latitude.has_value()
-        &&
-        aircraft.longitude.has_value()
-    )
-    {
-        std::cout
-            << aircraft.latitude.value()
-            << ", "
-            << aircraft.longitude.value();
+    if(aircraft.latitude.has_value() && aircraft.longitude.has_value()){
+        std::cout << aircraft.latitude.value() << ", " << aircraft.longitude.value();
     }
-    else
-    {
-        std::cout
-            << "Unknown";
+    else{
+        std::cout << "Unknown";
     }
 
-    std::cout
-        << '\n';
+    std::cout << '\n';
 
+    std::cout << "Ground Speed: ";
 
-    std::cout
-        << "Ground Speed: ";
-
-    if (
-        aircraft.ground_speed.has_value()
-    )
-    {
-        std::cout
-            << aircraft.ground_speed.value()
-            << " knots";
+    if(aircraft.ground_speed.has_value()){
+        std::cout << aircraft.ground_speed.value() << " knots";
     }
-    else
-    {
-        std::cout
-            << "Unknown";
+    else{
+        std::cout << "Unknown";
     }
 
-    std::cout
-        << '\n';
+    std::cout << '\n';
 
+    std::cout << "Track: ";
 
-    std::cout
-        << "Track: ";
-
-    if (
-        aircraft.track.has_value()
-    )
-    {
-        std::cout
-            << aircraft.track.value()
-            << " degrees";
+    if(aircraft.track.has_value()){
+        std::cout << aircraft.track.value() << " degrees";
     }
-    else
-    {
-        std::cout
-            << "Unknown";
+    else{
+        std::cout << "Unknown";
     }
 
-    std::cout
-        << '\n';
+    std::cout << '\n';
 
-
-    std::cout
-        << "Last Seen: "
-        << aircraft
-            .seconds_since_last_seen()
-            .count()
-        << " seconds ago\n";
+    std::cout << "Last Seen: " << aircraft.seconds_since_last_seen().count() << " seconds ago\n";
 }
 
 
-void print_all_aircraft(
-    const AircraftManager& manager
-)
-{
-    std::cout
-        << "=================================\n";
+void print_all_aircraft(const AircraftManager& manager){
+    std::cout << "=================================\n";
+    std::cout << "       CURRENT AIRCRAFT\n";
+    std::cout << "=================================\n";
+    std::cout << "Aircraft tracked: " << manager.aircraft_count() << "\n\n";
 
-    std::cout
-        << "       CURRENT AIRCRAFT\n";
+    auto aircraft_snapshot = manager.get_all_aircraft();
 
-    std::cout
-        << "=================================\n";
-
-
-    std::cout
-        << "Aircraft tracked: "
-        << manager.aircraft_count()
-        << "\n\n";
-
-
-    auto aircraft_snapshot =
-        manager.get_all_aircraft();
-
-
-    for (
-        const auto& [hex_ident, aircraft]
-        :
-        aircraft_snapshot
-    )
-    {
-        print_aircraft(
-            aircraft
-        );
+    for(const auto& [hex_ident, aircraft] : aircraft_snapshot){
+        print_aircraft(aircraft);
     }
 }
 
 
-int main()
-{
-    Debug::log(
-        "Main",
-        "Program started"
-    );
+int main(){
+    Debug::log("Main", "Program started");
 
-
-    try
-    {
+    try{
         AircraftManager manager;
 
+        Debug::log("Main", "Starting UDP receiver");
 
-        Debug::log(
-            "Main",
-            "Starting UDP receiver"
-        );
+        UdpReceiver receiver(4000);
 
+        Debug::success("Main", "Listening for aircraft data");
 
-        UdpReceiver receiver(
-            4000
-        );
-
-
-        Debug::success(
-            "Main",
-            "Listening for aircraft data"
-        );
-
-
-        // -----------------------------------------
         // Receiver thread
-        // -----------------------------------------
+        std::thread receiver_thread([&](){
+            while(true){
+                std::string line = receiver.read_line();
 
-        std::thread receiver_thread(
-            [&]()
-            {
-                while (true)
-                {
-                    std::string line =
-                        receiver.read_line();
+                BaseStationMessage message(line);
 
-
-                    BaseStationMessage message(
-                        line
-                    );
-
-
-                    manager.process_message(
-                        message
-                    );
-                }
+                manager.process_message(message);
             }
-        );
+        });
 
-
-        // -----------------------------------------
         // Main display loop
-        // -----------------------------------------
-
-        while (true)
-        {
-            manager.remove_stale_aircraft(
-                60
-            );
-
+        while(true){
+            manager.remove_stale_aircraft(60);
 
             clear_screen();
 
+            print_all_aircraft(manager);
 
-            print_all_aircraft(
-                manager
-            );
-
-
-            std::this_thread::sleep_for(
-                std::chrono::seconds(1)
-            );
+            std::this_thread::sleep_for(std::chrono::seconds(1));
         }
-
 
         receiver_thread.join();
     }
-    catch (
-        const std::exception& exception
-    )
-    {
-        Debug::error(
-            "Main",
-            exception.what()
-        );
-
+    catch(const std::exception& exception){
+        Debug::error("Main", exception.what());
 
         return 1;
     }
-
 
     return 0;
 }
